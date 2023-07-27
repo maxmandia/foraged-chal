085f57e0720d45022f200aeeb4933f5d71190d10
MaxMandia <mmandia511@gmail.com>
2023-07-27 09:14:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Foraged Challenge
---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(3001, () => console.log("Server running on port 3001"));
